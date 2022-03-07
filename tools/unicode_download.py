b4fa6f5c6c4cb9d3f78b6575ee979d78402d212d
psyirius <psyirius@gmail.com>
2022-03-07 11:40:22 +0530

    unicode download shell script ported to python

---
+++ tools/unicode_download.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if sys.hexversion < 0x3080:
+    import platform
+
+    raise RuntimeError('Python 3.8 or higher is needed!\nGot %s instead.' % platform.python_version())
+
+import os
+from os import path
+from urllib import request
+
+
+def urljoin(*args) -> str:
+    return '/'.join(args)
+
+
+def mkdirp(_dir) -> None:
+    os.makedirs(_dir, exist_ok=True)
+
+
+unicode_public_url = f'https://unicode.org/Public'
+version = '14.0.0'
+
+base_url = urljoin(unicode_public_url, version, 'ucd')
+print(base_url)
+
+self_dir = path.dirname(__file__)
+dest_dir = path.join(self_dir, '..', 'data', 'unicode')
+
+files = (
+    'CaseFolding.txt',
+    'DerivedNormalizationProps.txt',
+    'PropList.txt',
+    'SpecialCasing.txt',
+    'CompositionExclusions.txt',
+    'ScriptExtensions.txt',
+    'UnicodeData.txt',
+    'DerivedCoreProperties.txt',
+    'NormalizationTest.txt',
+    'Scripts.txt',
+    'PropertyValueAliases.txt',
+    ('emoji/emoji-data.txt', 'emoji-data.txt'),
+)
+
+# Ensure output directory exists
+mkdirp(dest_dir)
+
+for file in files:
+    dest_file = file
+
+    if isinstance(file, tuple):
+        file, dest_file = file
+
+    url = urljoin(base_url, file)
+    dest = path.join(dest_dir, dest_file)
+
+    print(f'Downloading: {url} -> {path.relpath(dest, self_dir)}')
+
+    # Download file
+    request.urlretrieve(url, dest)
